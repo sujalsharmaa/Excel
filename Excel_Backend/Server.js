acddf87f9bebb9433a0e59d874b1bf14bb19a4bd
sujalsharmaa <sujalsharma151@gmail.com>
2025-03-16 11:28:53 +0530

    added origin

--- Excel_Backend/Server.js
+++ Excel_Backend/Server.js
@@ -60,13 +60,13 @@ app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); // Handle preflight requests
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://sujalsharma.in");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://sujalsharma.in");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 
 // 1. Session middleware must come before passport
